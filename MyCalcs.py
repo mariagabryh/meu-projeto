289a5ef6b111dae6ae0417a46fa3c8fd6c8e05a8
mariagabryh <clarinhagabry@gmail.com>
2024-09-06 19:10:49 -0300

    demonstração

---
+++ MyCalcs.py
@@ -0,0 +1,16 @@
+# Demonstração do uso de operadores...
+
+X = int(input("Digite um número:"))
+Y = int(input("Digite outro número:"))
+
+SOMA = X + Y 
+print("O resultado da soma é:", SOMA)
+
+SUBTRAÇÃO = X - Y
+print("O resultado da subtração é:", SUBTRAÇÃO)
+
+MULTIPlICAÇÃO = X * Y
+print ("O resultado da subtração é:", MULTIPlICAÇÃO)
+
+DIVISÃO = X/Y 
+print ("O resultado da divisão é:", DIVISÃO)
---
+++ MyOperators.py
@@ -0,0 +1,13 @@
+# Demonstração de expressões complexas
+
+# Declaração de variaveis 
+X = 10; Y= 20; Z = 30
+
+VALOR = X * (Y / 10 + Z / 10) - 25
+print("Oresultado da expressão #1 é:", VALOR)
+
+VALOR = ((Y - X) + (Z - X)) / 3
+print("O resultado da expressão #2 é:", VALOR)
+
+VALOR = (X + (Y * 2 + (Z / 10))) - 18
+print("O resultado da expressãp #3 é:", VALOR)
